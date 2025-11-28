40a9d852ef591366beba97eb38a3b3b61b4bfb45
samuelsurbakti <sammysurbakti@gmail.com>
2025-11-28 11:08:53 +0700

    add switch-lamp css and js

---
+++ storage/app/src/js/switch-lamp.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded",() => {
+	const b1 = new OnOffButton("#btn2");
+});
+
+class OnOffButton {
+	constructor(el) {
+		this.el = document.querySelector(el);
+		this.el?.addEventListener("click",this.power.bind(this));
+	}
+	power() {
+		const pressed = this.el?.getAttribute("aria-pressed") === "true";
+		this.el?.setAttribute("aria-pressed",`${!pressed}`);
+	}
+}
